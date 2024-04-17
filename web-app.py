cae17e8f30bc89c2e2443febc49854b8f50feaa7
Sainty717 <112627718+Sainty717@users.noreply.github.com>
2024-04-17 11:40:00 +0200

    Update web-app.py

--- web-app.py
+++ web-app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 nodes_file = "nodes.json"
 links_file = "links.json"
+key_file = "key.json"
 theme_file = "theme.json"
 
 def load_json(filename):
@@ -14,13 +15,14 @@ def load_json(filename):
         data = json.load(f)
     return data
 
-# Read initial nodes, links, and theme data
+# Read initial nodes, links, key, and theme data
 nodes = load_json(nodes_file)
 links = load_json(links_file)
+key = load_json(key_file)
 theme = load_json(theme_file)
 
 def check_file_changes():
-    global nodes, links, theme
+    global nodes, links, key, theme
     while True:
         # Check modification time of nodes file
         nodes_modified_time = os.path.getmtime(nodes_file)
@@ -36,9 +38,15 @@ def check_file_changes():
             nodes = load_json(nodes_file)
             links = load_json(links_file)
             print("Links file updated. Reloading data...")
+        # Check modification time of key file
+        key_modified_time = os.path.getmtime(key_file)
+        if key_modified_time > max(nodes_modified_time, links_modified_time):
+            # Key file has been modified
+            key = load_json(key_file)
+            print("Key file updated. Reloading data...")
         # Check modification time of theme file
         theme_modified_time = os.path.getmtime(theme_file)
-        if theme_modified_time > os.path.getmtime(nodes_file) or theme_modified_time > os.path.getmtime(links_file):
+        if theme_modified_time > max(nodes_modified_time, links_modified_time, key_modified_time):
             # Theme file has been modified
             theme = load_json(theme_file)
             print("Theme file updated. Reloading data...")
@@ -46,7 +54,7 @@ def check_file_changes():
 
 @app.route('/')
 def index():
-    return render_template('app-index.html', nodes=nodes, links=links, theme=theme)
+    return render_template('app-index.html', nodes=nodes, links=links, key=key, theme=theme)
 
 @app.route('/nodes')
 def get_nodes():
@@ -55,6 +63,10 @@ def get_nodes():
 @app.route('/links')
 def get_links():
     return jsonify(links)
+
+@app.route('/key')
+def get_key():
+    return jsonify(key)
 
 if __name__ == '__main__':
     # Start the file change checker thread
